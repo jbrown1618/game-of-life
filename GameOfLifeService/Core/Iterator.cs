7a92e8372cb76ff25b3925715d507ad2bcd8b01a
Josh Brown <josh.brown@sas.com>
2020-05-03 11:34:05 -0400

    refactor: Clean up the Iterator code.
    Broke up the quick-and-dirty initial implementation into more
    methods and used a more elegant solution for counting neighbors.

--- GameOfLifeService/Core/Iterator.cs
+++ GameOfLifeService/Core/Iterator.cs
@@ -1,50 +1,63 @@
+using System;
 using System.Collections.Generic;
 
 namespace GameOfLifeService.Core
 {
     public static class Iterator
     {
-        public static GameOfLifeState Iterate(GameOfLifeState previous)
+        public static GameOfLifeState Iterate(GameOfLifeState state)
         {
-            if (Validator.Validate(previous).Count > 0)
+            ISet<string> errors = Validator.Validate(state);
+            if (errors.Count > 0)
             {
-                throw new System.ArgumentException("The starting state must be valid");
+                throw new System.ArgumentException("The starting state must be valid:\n" + String.Join("\n", errors));
             }
 
-            ISet<(ushort Row, ushort Col)> livingCells = previous.LiveCells;
             ISet<(ushort Row, ushort Col)> survivingCells = new HashSet<(ushort Row, ushort Col)>();
 
             // This is nowhere near the fastest algorithm, but whatever.
-            for (ushort i = 0; i < previous.Height; i++)
+            for (ushort i = 0; i < state.Height; i++)
             {
-                for (ushort j = 0; j < previous.Width; j++)
+                for (ushort j = 0; j < state.Width; j++)
                 {
-                    bool hasTopLeft = livingCells.Contains(NormalizeCoordinates((i - 1, j - 1), previous));
-                    bool hasTop = livingCells.Contains(NormalizeCoordinates((i - 1, j), previous));
-                    bool hasTopRight = livingCells.Contains(NormalizeCoordinates((i - 1, j + 1), previous));
-                    bool hasLeft = livingCells.Contains(NormalizeCoordinates((i, j - 1), previous));
-                    bool living = livingCells.Contains((i, j));
-                    bool hasRight = livingCells.Contains(NormalizeCoordinates((i, j + 1), previous));
-                    bool hasBottomLeft = livingCells.Contains(NormalizeCoordinates((i + 1, j - 1), previous));
-                    bool hasBottom = livingCells.Contains(NormalizeCoordinates((i + 1, j), previous));
-                    bool hasBottomRight = livingCells.Contains(NormalizeCoordinates((i + 1, j + 1), previous));
-                    int numNeighbors = (hasTopLeft ? 1 : 0)
-                            + (hasTop ? 1 : 0)
-                            + (hasTopRight ? 1 : 0)
-                            + (hasLeft ? 1 : 0)
-                            + (hasRight ? 1 : 0)
-                            + (hasBottomLeft ? 1 : 0)
-                            + (hasBottom ? 1 : 0)
-                            + (hasBottomRight ? 1 : 0);
-
-                    if (numNeighbors == 3 || (living && numNeighbors == 2))
+                    if (ShouldSurvive((i, j), state))
                     {
                         survivingCells.Add((i, j));
                     }
                 }
             }
 
-            return new GameOfLifeState(previous.Width, previous.Height, survivingCells);
+            return new GameOfLifeState(state.Width, state.Height, survivingCells);
+        }
+
+        private static bool ShouldSurvive((ushort Row, ushort Col) coords, GameOfLifeState state)
+        {
+            bool living = state.LiveCells.Contains(coords);
+            uint numNeighbors = CountNeighbors(coords, state);
+            return numNeighbors == 3 || (living && numNeighbors == 2);
+        }
+
+        private static uint CountNeighbors((ushort Row, ushort Col) coords, GameOfLifeState state)
+        {
+            ushort i = coords.Row;
+            ushort j = coords.Col;
+            uint neighborCount = 0;
+
+            int[] offsets = new int[] { -1, 0, 1 };
+            foreach (int rowOffset in offsets)
+            {
+                foreach (int colOffset in offsets)
+                {
+                    if (rowOffset == 0 && colOffset == 0) continue; // Skip self
+
+                    (ushort Row, ushort Col) neighborCoords = NormalizeCoordinates((i + rowOffset, j + colOffset), state);
+                    if (state.LiveCells.Contains(neighborCoords))
+                    {
+                        neighborCount++;
+                    }
+                }
+            }
+            return neighborCount;
         }
 
         private static (ushort Row, ushort Col) NormalizeCoordinates((int Row, int Col) coords, GameOfLifeState state)
