a54deaed1060b182a3ec5ca549b3995cc4c13159
Josh Brown <josh.brown@sas.com>
2020-05-03 20:36:08 -0400

    test: Add tests for GameOfLifeController

--- GameOfLifeService/Controllers/GameOfLifeController.cs
+++ GameOfLifeService/Controllers/GameOfLifeController.cs
@@ -1,6 +1,4 @@
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using GameOfLifeService.Core;
 using GameOfLifeService.DTO;
 
@@ -11,21 +9,19 @@ namespace GameOfLifeService.Controllers
     public class GameOfLifeController : ControllerBase
     {
 
-        private readonly ILogger<GameOfLifeController> _logger;
         private IIterator _iterator;
 
-        public GameOfLifeController(ILogger<GameOfLifeController> logger, IIterator iterator)
+        public GameOfLifeController(IIterator iterator)
         {
-            _logger = logger;
             _iterator = iterator;
         }
 
         [HttpGet]
         public IActionResult Get(int width, int height)
         {
-            if (width <= 0 || height <= 0)
+            if (width < 0 || height < 0)
             {
-                return StatusCode(500, "Width and Height must be > 0");
+                return StatusCode(500, "Width and Height must be nonnegative");
             }
             if (height > ushort.MaxValue || width > ushort.MaxValue)
             {
